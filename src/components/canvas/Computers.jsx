4dcca159397ecad76f6ff6947c13e15e7310f904
lll <798799155@qq.com>
2024-01-01 21:23:15 +0800

    更改vite的相对路径

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({isMobile}) => {
-  const computer = useGLTF("../../../public/otter/scene.gltf");
+  const computer = useGLTF("./public/otter/scene.gltf");
 
   return(
     <mesh>
